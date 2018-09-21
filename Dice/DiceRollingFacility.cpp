be0309d8e605ed5383f3dedffe08a250526c8be7
YaroH <43189766+YaroH@users.noreply.github.com>
2018-09-20 23:22:52 -0400

    Update DiceRollingFacility.cpp

--- Dice/DiceRollingFacility.cpp
+++ Dice/DiceRollingFacility.cpp
@@ -9,73 +9,71 @@ using namespace std;
 
 DiceRollingFacility::DiceRollingFacility(){
 	totalDiceRolled = 0;
-	std::vector<int> sortedContainer;
-	int percentArr[6]; 
-	initPercentArr();
+	double percentArr[6]; 
+	initalizePercentArr();
 }
 
-/*  Main function of the Dice Rolling Facility object.
+/*  Main method of the Dice Rolling Facility object.
 	It enables player to decide how many dice are being rolled (from 1 to 3).
-	Retuns values of the dice to the sorted container and updates percentages of all individual rolles.
+	Retuns sorted container(vector) with dice values and updates percentages.
 */
-void DiceRollingFacility::rollDice() {
-	int numberOfDice;
-
-	cout << "How many dice you would like to roll (from 1 to 3 dice)? ";
+vector<int> DiceRollingFacility::rollDice() {
+	int numberOfDice = 3;	
+	vector <int> sortContainer;
 
 	// Number of dice user wants to roll
+	cout << "How many dice you would like to roll (from 1 to 3 dice)? ";
 	do {
 		cin >> numberOfDice;
-		if (numberOfDice < 1 || numberOfDice > 3)
+	//	if (numberOfDice < 1 || numberOfDice > 3)
 			cout << "Number of dice must be betwen 1 and 3 ";
 	} while (numberOfDice < 1 || numberOfDice > 3);
 
-	// Update total number of dice rolled
-	totalDiceRolled += numberOfDice;
-
 	// Get values of dice and store in the Sorted Container/Update percentage array
 	for (int i = 0; i < numberOfDice; i++) {
+		// Update total number of dice rolled
+		totalDiceRolled++;
+
+		// Create dice object and roll it
 		Dice obj;
-		addToContainer(obj.getDiceValue());
+		// Stor result of the roll in container and update percentages
+		sortContainer.push_back(obj.getDiceValue());
 		addToPercentArr(obj.getDiceValue());
 	}
-	sortContainer();
+	// Sort container
+	std::sort(sortContainer.begin(), sortContainer.end());
+	return sortContainer;
 }
 
 // Percentage array
-void DiceRollingFacility::initPercentArr() {
-	for (int i = 0; i < 6; ++i)
-		percentArr[i] = 0;
-}
-void DiceRollingFacility::getPercentOfValues() {
-	for (int i = 0; i < 6; ++i) {
-		cout << i + 1 << " was rolled " << (double) percentArr[i] / totalDiceRolled * 100 << " % of the time." << endl;
-	}
-}
 void DiceRollingFacility::displayPercentArr() {
 	for (int i = 0; i < 6; ++i) {
-		cout << percentArr[i] << endl;
+		cout << i + 1 << " was rolled " << percentArr[i] << " % of the time." << endl;
 	}
+}
+void DiceRollingFacility::getPercentRoll(int roll) {
+	if (roll > 6 || roll < 1) {
+		cout << "Dice roll must be between 1 and 6" << endl;
+		exit(1);
+	}
+	cout << roll << " was rolled " << percentArr[roll-1] << " % of the time." << endl;
+}
+void DiceRollingFacility::initalizePercentArr() {
+	for (int i = 0; i < 6; ++i)
+		percentArr[i] = 0.0;
 }
 void DiceRollingFacility::addToPercentArr(int value) {
+	// Get from % to Double
+	if (getTotalDiceRolled() > 1) {
+		for (int i = 0; i < 6; ++i) {
+			percentArr[i] = percentArr[i] / 100 * (getTotalDiceRolled() - 1);
+		}
+	}
+	// Update roll and convert back to %
 	percentArr[value - 1]++;
-}
-
-// Sorted Container
-void DiceRollingFacility::addToContainer(int value) {
-	sortedContainer.insert(sortedContainer.begin(), value);
-}
-void DiceRollingFacility::displayContainer() {
-	for (const int &number : sortedContainer) {
-		cout << number << endl;
+	for (int i = 0; i < 6; ++i) {
+		percentArr[i] = percentArr[i] / getTotalDiceRolled() * 100;
 	}
 }
-void DiceRollingFacility::clearContainer() {
-	sortedContainer.clear();
-}
-void DiceRollingFacility::sortContainer() {
-	std::sort(sortedContainer.begin(), sortedContainer.end());
-}
-
 // Total number of rolled dice
-void DiceRollingFacility::getTotalDiceRolled() { cout << "Total number of dice rolled is: " << totalDiceRolled << endl; }
+int DiceRollingFacility::getTotalDiceRolled() { return totalDiceRolled; }
