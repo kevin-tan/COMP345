ff003bd24954e82cb3f5ee19697d9553d413d893
YaroH <43189766+YaroH@users.noreply.github.com>
2018-09-20 23:25:04 -0400

    Update Main.cpp

--- Dice/Main.cpp
+++ Dice/Main.cpp
@@ -8,25 +8,39 @@
 
 using namespace std;
 
+void out(vector<int> MyV);
 int main() {
-	srand(time(0));
+	srand(time(0));	// Unique pattern for the rand() function. MUST BE first statement in main function!
+	vector<int> test;
 
 	DiceRollingFacility obj;
+	
+	for (int i = 0; i < 3; ++i) {
+		cout << "Roll" << endl;
+		test = obj.rollDice();
+		cout << "Sotred container is ";
+		out(test);
 
-	obj.rollDice();
-	obj.displayContainer();
-
-	obj.rollDice();
-	obj.displayContainer();
-
-	obj.getTotalDiceRolled();
-	obj.getPercentOfValues();
-
-
-
-
-
-
+		cout << " \nPercentage array is: " << endl;
+		obj.displayPercentArr();
+		cout << endl;
+		}
+	obj.getPercentRoll(6);
+	
+	// Test for the equal share of rolls
+		for (int i = 0; i < 1000; ++i) {
+			obj.rollDice();
+		}
+		obj.displayPercentArr();
+	
 
 	return 0;
+}
+
+	// Helper method to display container
+	void out(vector<int> MyV){
+		for (unsigned int i = 0; i < MyV.size(); ++i) {
+			cout << MyV.at(i) << " ";
+		}
+
 }
